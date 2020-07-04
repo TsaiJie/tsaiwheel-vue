d0bfbf670601bd6a8c7b9caefc5bd7c0f785da5d
tsai <mengjietsai@163.com>
2020-07-04 16:20:02 +0800

    button 按钮使用iconfont 使用类实现图标左右 icon iconPosition

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,29 @@
 <template>
-	<button class="t-button">按钮</button>
+	<button class="t-button" :class="{[`icon-${iconPosition}`]: true}">
+		<svg class="icon" v-if="icon">
+			<use :xlink:href=`#i-${icon}`></use>
+		</svg>
+		<div class="content">
+			<slot></slot>
+		</div>
+
+	</button>
 </template>
 
 <script>
   export default {
+    props:['icon', 'iconPosition']
   }
 </script>
 
 <style lang="scss">
 	.t-button {
+		display: inline-flex;
+		/*垂直居中*/
+		justify-content: center;
+		align-items: center;
+		vertical-align: middle;
+
 		font-size: var(--font-size);
 		height: var(--button-height);
 		padding: 0 1em;
@@ -24,6 +39,26 @@
 		&:focus {
 			outline: none;
 		}
-	}
+		/*默认icon在前面 content在后面*/
+		> .icon{
+			order: 1;
+			margin-right: .3em;
+		}
+		> .content {
+			order: 2;
+		}
+		/*如果iconPosition = right 则 content在前 icon在后*/
+		&.icon-right {
+			> .icon {
+				order: 2;
+				margin-left: .3em;
+				margin-right: 0;
 
+			}
+			> .content {
+				order: 1
+			}
+		}
+	}
+Í
 </style>
