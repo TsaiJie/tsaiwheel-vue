685011fe4b80cd8964ca71ce96431a9cd082408b
tsai <mengjietsai@163.com>
2020-07-04 17:32:02 +0800

    创建button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 Vue.component('t-button', Button)
 Vue.component('t-icon', Icon)
+Vue.component('t-button-group', ButtonGroup)
 new Vue({
   el: '#app',
   data:{
---
+++ src/button-group.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="t-button-group">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      for (let node of this.$el.children){
+        let name = node.nodeName.toLowerCase()
+        if(name !== 'button') {
+          console.warn(`t-button-group的子元素应该全部是 t-button， 但是你写的是${name}`)
+				}
+			}
+    }
+  }
+</script>
+
+<style  lang="scss" scoped>
+.t-button-group {
+	display: inline-flex;
+	vertical-align: middle;
+	> .t-button {
+		border-radius: 0;
+		&:first-child {
+			border-top-left-radius: var(--border-radius);
+			border-bottom-left-radius: var(--border-radius);
+		}
+		&:last-child {
+			border-top-right-radius: var(--border-radius);
+			border-bottom-right-radius: var(--border-radius);
+		}
+		&:not(:first-child){
+			margin-left: -1px;
+		}
+		&:hover {
+			position: relative;
+			z-index: 1;
+		}
+	}
+}
+</style>
