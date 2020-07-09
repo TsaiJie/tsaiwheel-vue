2c02f428c03d5d710dd56a39c3f396277d35883f
tsai <mengjietsai@163.com>
2020-07-09 18:19:20 +0800

    添加footer header layout content sider组件

---
+++ src/layout/content.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "WheelContent"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/layout/header.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: "WheelHeader"
+  }
+</script>
+
+<style scoped>
+
+</style>
