96972eb2c34b51cc4bd0763c9ce9a2a81ca61506
tsai <mengjietsai@163.com>
2020-07-16 00:00:07 +0800

    tabs-item添加 disabled

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -14028,7 +14028,8 @@ var _default = {
   computed: {
     classes: function classes() {
       return {
-        active: this.active
+        active: this.active,
+        disabled: this.disabled
       };
     }
   },
@@ -14040,7 +14041,8 @@ var _default = {
     });
   },
   methods: {
-    xxx: function xxx() {
+    onClick: function onClick() {
+      if (this.disabled) return;
       this.eventBus.$emit('update:selected', this.name, this);
     }
   }
@@ -14060,7 +14062,11 @@ exports.default = _default;
   var _c = _vm._self._c || _h
   return _c(
     "div",
-    { staticClass: "tabs-item", class: _vm.classes, on: { click: _vm.xxx } },
+    {
+      staticClass: "tabs-item",
+      class: _vm.classes,
+      on: { click: _vm.onClick }
+    },
     [_vm._t("default")],
     2
   )
--- src/tabs/tabs-item.vue
+++ src/tabs/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="tabs-item" @click="xxx" :class="classes">
+	<div class="tabs-item" @click="onClick" :class="classes">
 		<slot></slot>
 	</div>
 </template>
@@ -7,54 +7,60 @@
 <script>
   export default {
     name: "WheelTabsItem",
-		inject: ['eventBus'],
-		data(){
+    inject: ['eventBus'],
+    data() {
       return {
         active: false
-			}
-		},
-		props: {
+      }
+    },
+    props: {
       disabled: {
         type: Boolean,
-				default: false
-			},
-			name: {
+        default: false
+      },
+      name: {
         type: String | Number,
-				required: true
-			}
-		},
-		computed: {
-      classes(){
-        return {
-          active: this.active
-				}
-			}
-		},
-		created() {
-      this.eventBus.$on('update:selected', (name)=>{
-        this.active = name === this.name;
-			})
+        required: true
+      }
     },
-		methods: {
-      xxx(){
+    computed: {
+      classes() {
+        return {
+          active: this.active,
+          disabled: this.disabled
+        }
+      }
+    },
+    created() {
+      this.eventBus.$on('update:selected', (name) => {
+        this.active = name === this.name;
+      })
+    },
+    methods: {
+      onClick() {
+        if (this.disabled) return
         this.eventBus.$emit('update:selected', this.name, this)
-			}
-		}
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
 	$blue: skyblue;
+	$disabled-text-color: grey;
 	.tabs-item {
-	flex-shrink: 0;
-	padding: 0 2em;
-	height: 100%;
-	display: flex;
-	align-items: center;
-	&.active {
-		background: #eee;
-		color: $blue;
+		flex-shrink: 0;
+		padding: 0 2em;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		&.active {
+			background: #eee;
+			color: $blue;
+		}
+		&.disabled {
+			color: $disabled-text-color;
+		}
+		cursor: pointer;
 	}
-	cursor: pointer;
-}
 </style>
